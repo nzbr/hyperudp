68bf54a5fc27f4a07251cc6e3b4fe43898bfbfbe
nzbr <mail@nzbr.de>
2020-04-24 12:30:25 +0200

    Fix typos in help text

--- hyperudp.go
+++ hyperudp.go
@@ -10,9 +10,9 @@ import (
 //Args stores command line argument values
 type Args struct {
 	IP       string `arg:"positional,required" help:"Hyperion Server IP"`
-	Port     int    `help:"Hyperion FlatBuffer Port"`
+	Port     int    `help:"Hyperion Protocol Buffer Port"`
 	UDPPort  int    `help:"UDP Listen Port"`
-	Name     string `help:"Name of this stream un Hyperion"`
+	Name     string `help:"Name of this stream in Hyperion"`
 	Priority int32  `help:"Priority of this stream in Hyperion"`
 	Duration int32  `help:"How long a single sent frame is displayed if nothing replaces it (in milliseconds)"`
 }
